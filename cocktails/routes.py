9afe4efb80e6fa02cbcc5017b670587ae47390b2
rhysmoggs <rhysmoggs@hotmail.com>
2022-05-23 21:34:59 +0000

    bug fixing

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -2,6 +2,7 @@ from flask import (
     flash, render_template,
     request, redirect, session, url_for)
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 from cocktails import app, db, mongo
 from cocktails.models import Category, Users
 
