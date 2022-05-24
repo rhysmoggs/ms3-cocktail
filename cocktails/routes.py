33005932b19c21461da51da6f99d80c750eeea77
rhysmoggs <rhysmoggs@hotmail.com>
2022-05-24 14:55:27 +0000

    further testing of mongo db

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -1,6 +1,4 @@
-from flask import (
-    flash, render_template,
-    request, redirect, session, url_for)
+from flask import flash, render_template, request, redirect, session, url_for
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from cocktails import app, db, mongo
