c1e955ad7ffb05b9ae919cecd9172fee69eb493d
rhysmoggs <rhysmoggs@hotmail.com>
2022-05-30 07:04:07 +0000

    create registration page

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -10,3 +10,31 @@ from cocktails.models import Category, Users
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = Users.query.filter(Users.user_name == \
+                                           request.form.get("username").lower()).all()
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        # create a dictionary
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        # return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
