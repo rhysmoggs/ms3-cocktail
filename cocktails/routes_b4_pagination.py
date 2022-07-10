ae581897a2beb4c67b785b33b6d9a9003c48e50b
rhysmoggs <rhysmoggs@hotmail.com>
2022-07-10 11:24:03 +0000

    pagination installed and applied

--- cocktails/routes_b4_pagination.py
+++ cocktails/routes_b4_pagination.py
@@ -1,5 +1,4 @@
 from flask import flash, render_template, request, redirect, session, url_for
-from flask_paginate import Pagination, get_page_args
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from cocktails import app, db, mongo
@@ -21,50 +20,10 @@ def home():
     return render_template("home.html", cocktails=cocktails, categories=categories)
 
 
-# @app.route('/')
-# def index():
-#     page, per_page, offset = get_page_args(page_parameter='page',
-#                                            per_page_parameter='per_page')
-#     total = len(users)
-    # pagination_users = get_users(offset=offset, per_page=per_page)
-    # pagination = Pagination(page=page, per_page=per_page, total=total,
-    #                         css_framework='materializecss')
-    # return render_template('index.html',
-    #                        users=pagination_users,
-    #                        page=page,
-    #                        per_page=per_page,
-    #                        pagination=pagination,
-    #                        )
-
-
-# users = list(range(100))
-
-
-
-# def get_users(offset=0, per_page=10):
-#     return users[offset: offset + per_page]
-
-
 @app.route("/all_cocktails")
 def all_cocktails():
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    per_page = 9
-    offset = (page - 1) * per_page
-    # total = mongo.db.cocktails.find().count()
     cocktails = list(mongo.db.cocktails.find())
-    total = len(cocktails)
-    cocktails_paginated = cocktails[offset: offset + per_page]
-    # get_cocktails(offset=0, per_page=10) = cocktails[offset: offset + per_page]
-    # pagination_users = cocktails_paginated(offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='materializecss')
-    return render_template("all_cocktails.html",
-                           cocktails=cocktails_paginated,
-                        #    users=pagination_users,
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination)
+    return render_template("all_cocktails.html", cocktails=cocktails)
 
 
 @app.route("/filter_category/<int:category_id>")
